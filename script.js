4be64e013f4bc535931b5a9c29d8cf3142a2d6b7
ATC <tennissg@web.de>
2026-03-04 01:48:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function loadComponent(id, file) {
 
 loadComponent('nav-placeholder', 'nav.html');
 loadComponent('footer-placeholder', 'footer.html');
+
+    // <!-- Für das Davis-Cup-Clubturnier --> !
+
+let scoreBlue = 0;
+let scoreRed = 0;
+
+function updateScore(team) {
+    if (team === 'blue') {
+        scoreBlue++;
+        document.getElementById('score-blue').innerText = scoreBlue;
+    } else {
+        scoreRed++;
+        document.getElementById('score-red').innerText = scoreRed;
+    }
+}
+
+function resetScore() {
+    if(confirm("Punktestand wirklich zurücksetzen?")) {
+        scoreBlue = 0;
+        scoreRed = 0;
+        document.getElementById('score-blue').innerText = "0";
+        document.getElementById('score-red').innerText = "0";
+    }
+}
